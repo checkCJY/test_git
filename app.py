0badb2ffc1778ee8c450b3b311beccd1296fc05b
JaeYong Choi <sapla@naver.com>
2026-01-10 12:40:13 +0900

    add app code

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="Day10 Streamlit", layout="wide")
 st.title("🏙️ Streamlit Day10 시작")
 st.write("폴더 생성 → uv 환경 세팅 → 실행 성공까지 완료!")
+
+st.write("코드를 수정하였습니다.")
